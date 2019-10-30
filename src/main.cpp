234a8c2ba78d077e77a9c3942d15eea8789f7317
Bence Nagy <bence657@gmail.com>
2019-10-30 22:07:39 +0100

    Cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-#include <thread>
-#include <functional>
 
-#include "data_registry.hpp"
-#include "registry_entries.hpp"
+int main() {
 
-void writer_main() {
-
-    data_registry& registry = data_registry::get_instance();
-
-    while (true) {
-
-        sensor_data* sd = (sensor_data*) registry.acquire_data(SENSOR_DATA_ID);
-        sd->acceleration++;
-        sd->distance++;
-    
-        registry.release_data(SENSOR_DATA_ID);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-}
-
-int main () {
-
-    std::thread writer_thread(writer_main);
-    
-    data_registry& registry = data_registry::get_instance();
-    while (true) {
-
-        sensor_data* sd = (sensor_data*) registry.acquire_data(SENSOR_DATA_ID);
-        std::cout << "Acceleration: " << sd->acceleration << " Distance: " << sd->distance << std::endl;
-    
-        registry.release_data(SENSOR_DATA_ID);
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
-    }
-
-    writer_thread.join();
-
+    std::cout << "Central module main" << std::endl;
     return 0;
 }
