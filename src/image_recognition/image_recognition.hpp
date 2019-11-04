7f38db87b25e4768a80283eaf3fddce11b967cac
Bence Nagy <bence657@gmail.com>
2019-11-04 12:15:55 +0100

    Added comments to main functions

--- src/image_recognition/image_recognition.hpp
+++ src/image_recognition/image_recognition.hpp
@@ -1,6 +1,9 @@
 #ifndef CM_IMAGE_RECOGNITION_H
 #define CM_IMAGE_RECOGNITION_H
 
+/**
+ * Main function ran by the image recognition thread
+ */
 void image_recognition_main();
 
 #endif
--- src/logging/logging.hpp
+++ src/logging/logging.hpp
@@ -1,6 +1,12 @@
 #ifndef CM_LOGGING_H
 #define CM_LOGGING_H
 
+
+/**
+ * Main function ran by the logging thread
+ */
 void logging_thread_main();
+
+
 
 #endif
