a753395cbcd3664afbbe8bbb14c9eaf7b50931fd
Bence Nagy <bence657@gmail.com>
2019-11-11 14:52:28 +0100

    Added new data registry entry for control change data

--- src/data_registry/registry_entries.hpp
+++ src/data_registry/registry_entries.hpp
@@ -3,10 +3,16 @@
 
 
 #define SENSOR_DATA_ID "sensor_data"
+#define CONTROL_CHANGE_DATA_ID "control_change_data"
 
 struct sensor_data {
     float distance;
     float acceleration;
+};
+
+struct control_change_data {
+    char speed_delta;
+    char angle_delta;
 };
 
 #endif
