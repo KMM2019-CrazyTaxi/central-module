bd831b96ec151b439e48d3bcb8e11bab80f063f4
Bence Nagy <bence657@gmail.com>
2019-11-14 15:59:52 +0100

    Added more spi global constants

--- src/io/spi.hpp
+++ src/io/spi.hpp
@@ -6,6 +6,13 @@
 #define SPI_ERROR       0x55
 #define SPI_FINISHED    0x66
 #define SPI_RESTART     0x99
+#define SPI_ACK         0xEE
+
+#define SPI_CONTROL_INIT_MSG_SIZE       1
+#define SPI_CONTROL_DATA_MSG_SIZE       3
+#define SPI_CONTROL_FINISH_MSG_SIZE     1
+
+#define SPI_FAILED_WAIT_MS 1
 
 #define SPI_FREQ    1000000 / 32
 #define SPI_CONTROL 0
