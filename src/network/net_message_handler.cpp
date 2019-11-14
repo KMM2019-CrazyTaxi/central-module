ae26fd93fb1a7898e29004ec39b0d6bfc22e2678
Bence Nagy <bence657@gmail.com>
2019-11-14 19:46:16 +0100

    Implemented message handling for speed and angle setting

---
+++ src/network/net_message_handler.cpp
@@ -0,0 +1,44 @@
+
+#include "net_message_handler.hpp"
+#include "data_registry.hpp"
+#include "packet_ids.hpp"
+
+void handle_packets(const std::vector<packet>& packets) {
+
+    for (const packet& p : packets) {
+        handle_packet(p);
+    }
+
+}
+
+void handle_packet(const packet& p) {
+
+    data_registry& registry = data_registry::get_instance();
+
+    switch(p.get_type()) {
+        case SEND_MAX_SPEED:
+
+            uint8_t speed = p[0];
+
+            control_change_data* registry_entry = 
+                (control_change_data*) registry.acquire_data(CONTROL_CHANGE_DATA_ID);
+
+            registry_entry->speed = speed;
+
+            registry.release_data(CONTROL_CHANGE_DATA_ID);
+            break;
+        
+        case REQUEST_TURN:
+            uint8_t angle = p[0];
+
+            control_change_data* registry_entry = 
+                (control_change_data*) registry.acquire_data(CONTROL_CHANGE_DATA_ID);
+
+            registry_entry->angle = angle;
+
+            registry.release_data(CONTROL_CHANGE_DATA_ID);
+            break;
+        default:
+            break;
+    }
+}
--- src/network/net_message_handler.hpp
+++ src/network/net_message_handler.hpp
@@ -1,6 +1,13 @@
 #ifndef CM_NET_MESSAGE_HANDLER_H
 #define CM_NET_MESSAGE_HANDLER_H
 
+#include <vector>
 
+#include "packet.hpp"
+
+
+void handle_packets(const std::vector<packet>& packets);
+
+void handle_packet(const packet& p);
 
 #endif
