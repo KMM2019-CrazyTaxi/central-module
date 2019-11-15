3e08f521e54b5978992de7574ea1b5b142bcb5bc
Bence Nagy <bence657@gmail.com>
2019-11-15 14:14:17 +0100

    Added documentation

--- src/network/net_message_handler.hpp
+++ src/network/net_message_handler.hpp
@@ -7,12 +7,19 @@
 
 
 /**
+ * Handles a vector of packets and returns a vector of packets that contains the answer
+ * to the packets.
  * 
+ * @param packets A std::vector containing the packets to handle
+ * @return A new vector containing the answer packets
  */
 std::vector<packet> handle_packets(const std::vector<packet>& packets);
 
 /**
+ * Handles a single packet
  * 
+ * @param p The packet to handle
+ * @return The answer to the packet p
  */
 packet handle_packet(const packet& p);
 
