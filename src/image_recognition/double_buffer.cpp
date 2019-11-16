11037ee1ac02b10727b7d4b131ddfefa118046a7
Bence Nagy <bence657@gmail.com>
2019-11-16 14:46:46 +0100

    Implemented a double buffer to use in image processing

---
+++ src/image_recognition/double_buffer.cpp
@@ -0,0 +1,101 @@
+
+#include "double_buffer.hpp"
+
+#include <algorithm>
+#include <mutex>
+
+#ifdef SHARED_ENABLED
+
+double_buffer::double_buffer(uint32_t size) : buffer_1(size), buffer_2(size) {
+
+    read_buffer = &buffer_1;
+    write_buffer = &buffer_2;
+
+}
+
+#else
+
+double_buffer::double_buffer(uint32_t size) {
+
+    read_buffer = new uint8_t[size]();
+    write_buffer = new uint8_t[size]();
+}
+
+#endif
+
+
+double_buffer::~double_buffer() {
+
+    #ifdef SHARED_ENABLED
+        read_buffer = nullptr;
+        write_buffer = nullptr;
+        buffer_1.dealloc();
+        buffer_2.dealloc();
+    #else
+        delete[] read_buffer;
+        delete[] write_buffer;
+    #endif
+
+}
+
+uint8_t double_buffer::read(uint32_t index) {
+
+    #ifdef SHARED_ENABLED
+        return (*read_buffer)[index];
+    #else
+        return read_buffer[index];
+    #endif
+
+}
+
+void double_buffer::write(uint32_t index, uint8_t value) {
+
+    #ifdef SHARED_ENABLED
+        (*write_buffer)[index] = value;
+    #else
+        write_buffer[index] = value;
+    #endif
+
+}
+
+const uint8_t* double_buffer::get_read_buffer() {
+
+    #ifdef SHARED_ENABLED
+        return read_buffer->getPointer();
+    #else
+        return read_buffer;
+    #endif
+}
+
+uint8_t* double_buffer::get_write_buffer() {
+
+    #ifdef SHARED_ENABLED
+        return write_buffer->getPointer();
+    #else
+        return write_buffer;
+    #endif
+}
+
+void double_buffer::swap_buffers() {
+
+    write_lock.lock();
+
+    std::swap(read_buffer, write_buffer);
+    image_processed = true;
+
+    write_lock.unlock();
+    new_image_condition.notify_one();
+
+}
+
+void double_buffer::wait_for_image() {
+
+    // Create unique lock from writer lock without locking it, since it is already locked by this thread
+    std::unique_lock lock(write_lock, std::adopt_lock);
+    new_image_condition.wait(lock, [this]{ return this->image_processed.load(); });
+    
+    image_processed = false;
+
+    // Release association with the writer_lock, note that it is still locked after this
+    lock.release();
+}
---
+++ src/image_recognition/double_buffer.hpp
@@ -0,0 +1,113 @@
+#ifndef CM_DOUBLE_BUFFER_H
+#define CM_DOUBLE_BUFFER_H
+
+#include <cstdint>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+#ifdef QPULIB
+    #include "QPULib.h"
+#endif
+
+#ifdef _QPULIB_H_
+    #define SHARED_ENABLED
+#endif
+
+/**
+ * A double buffer class used to allow synchronous image reading and writing
+ */
+class double_buffer {
+
+public:
+
+    /**
+     * Constructor that creates a double_buffer with a given size
+     * 
+     * @param size The size of the buffer in bytes
+     */
+    double_buffer(uint32_t size);
+
+    /**
+     * Destructor
+     */
+    ~double_buffer();
+
+    /**
+     * Reads a byte from the read-only buffer
+     * 
+     * @param index The index to read from
+     */
+    uint8_t read(uint32_t index);
+
+    /**
+     * Writes a byte to the write-only buffer
+     * 
+     * @param index The index to write to
+     * @param value The value to write
+     */
+    void write(uint32_t index, uint8_t value);
+
+    /**
+     * @return Constant pointer to the read-only buffer
+     */
+    const uint8_t* get_read_buffer();
+
+    /**
+     * @return Pointer to the write-only buffer
+     */
+    uint8_t* get_write_buffer();
+
+    /**
+     * Swaps the read and write buffers
+     */
+    void swap_buffers();
+
+    /**
+     * Calling this function blocks the calling thread until swap_buffers is called.
+     * NOTE: Keeps the write buffer locked, make sure to unlock it when you are done
+     */
+    void wait_for_image();
+
+
+private:
+
+    /**
+     * Explicitly deleted default constructor and copy and move semantics
+     */
+    double_buffer() = delete;
+    double_buffer(const double_buffer& other) = delete;
+    double_buffer(double_buffer&& other) = delete;
+    double_buffer& operator=(const double_buffer& other) = delete;
+    double_buffer& operator=(double_buffer&& other) = delete;
+
+    // Locks the write buffer 
+    std::mutex write_lock;
+
+    // Condition that wakes up camera thread when the image has been processed
+    std::condition_variable new_image_condition;
+
+    // Flag for new image
+    std::atomic_bool image_processed;
+
+    #ifdef SHARED_ENABLED
+
+        // Buffers are shared with the GPU
+        SharedArray<uint8_t> buffer_1;
+        SharedArray<uint8_t> buffer_2;
+        
+        // Pointers to the shared buffers to allow swapping
+        SharedArray<uint8_t>* read_buffer;
+        SharedArray<uint8_t>* write_buffer;
+
+    #else
+        
+        // CPU Only buffers
+        uint8_t* read_buffer;
+        uint8_t* write_buffer;
+
+    #endif
+
+};
+
+#endif
