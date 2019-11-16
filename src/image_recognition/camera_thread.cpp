6055676fc113681eab486557cfa5b6ee62e106a9
Bence Nagy <bence657@gmail.com>
2019-11-16 15:05:17 +0100

    Implemented a first draft of the taking of photos

--- src/image_recognition/camera_thread.cpp
+++ src/image_recognition/camera_thread.cpp
@@ -1,8 +1,26 @@
 
 #include "camera_thread.hpp"
 
+#include <thread>
+#include <chrono>
+
+#ifdef RASPICAM
+    #include <raspicam/raspicam.h>
+#endif
+
+#include "logging.hpp"
 
 void camera_thread_main(const std::atomic_bool& running, double_buffer& image_buffer) {
+    
+    #ifdef _RaspiCam_H_
+        raspicam::RaspiCam camera;
+
+        image_buffer = double_buffer(camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+
+        if (!camera.open()) queue_message("Failed to open camera.");
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+    #endif
 
     while (running) {
 
@@ -12,6 +30,12 @@ void camera_thread_main(const std::atomic_bool& running, double_buffer& image_bu
         image_buffer.wait_for_image();
         
         // Write image to buffer
+        #ifdef _RaspiCam_H_
+
+            camera.grab();
+            camera.retrieve(image_buffer.get_write_buffer(), raspicam::RASPICAM_FORMAT_RGB);
+
+        #endif
     }
 
 }
