bd7ab8173e8654240ecae274f3c56a7fd7bf8d04
Herman Appelgren <herap86@upp2-103.ad.liu.se>
2019-11-20 21:58:20 +0100

    When the camera isn't used, a set of test images is instead read by camera_thread.

--- src/image_recognition/camera_thread.cpp
+++ src/image_recognition/camera_thread.cpp
@@ -10,6 +10,18 @@
 
 #include "logging.hpp"
 
+#ifndef RASPICAM
+    #include <string>
+    #include <fstream>
+
+    #include "image_util.hpp"
+
+    const int N_TEST_IMAGES{8};
+    const std::string TEST_IMAGE_PATH = "./src/image_recognition/res/";
+    const std::string TEST_IMAGES[N_TEST_IMAGES] = {"1.ppm", "2.ppm", "3.ppm", "4.ppm",
+						    "5.ppm", "6.ppm", "7.ppm", "8.ppm"};
+#endif
+
 void camera_thread_main(const std::atomic_bool& running, double_buffer& image_buffer) {
     
     #ifdef _RaspiCam_H_
@@ -23,6 +35,12 @@ void camera_thread_main(const std::atomic_bool& running, double_buffer& image_bu
         if (!camera.open()) queue_message("Failed to open camera.");
 
         std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    #else
+
+	int n_image{};
+	image_buffer.resize(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
+	
     #endif
 
     image_buffer.lock_write_buffer();
@@ -40,6 +58,14 @@ void camera_thread_main(const std::atomic_bool& running, double_buffer& image_bu
             camera.grab();
             camera.retrieve(image_buffer.get_write_buffer(), raspicam::RASPICAM_FORMAT_RGB);
 
+        #else
+
+	    // Read sample images if camera isn't available.
+	    std::ifstream input{ TEST_IMAGE_PATH + TEST_IMAGES[n_image], std::ios::binary };
+	    read_image(image_buffer.get_write_buffer(), input);
+	    n_image = (n_image + 1) % N_TEST_IMAGES;
+	    input.close();
+	    
         #endif
     }
 
