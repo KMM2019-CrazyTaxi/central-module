cb85e7af43f0fdf7362348d959689bf3a4974b5a
Herman Appelgren <herap86@upp2-103.ad.liu.se>
2019-11-21 11:39:58 +0100

    Added QPU cursor class.

---
+++ src/image_recognition/qpu_cursor.cpp
@@ -0,0 +1,50 @@
+#ifdef QPU_MODE
+
+#include "qpu_cursor.hpp"
+
+void Cursor::init(Ptr<Float> p) {
+    gather(p);
+    current = 0;
+    cursor = p+16;
+}
+
+// Receive the first vector and fetch the second.
+// (prime the software pipeline)
+void Cursor::prime() {
+    receive(next);
+    gather(cursor);
+}
+
+// Receive the next vector and fetch another.
+void Cursor::advance() {
+    cursor = cursor+16;
+    prev = current;
+    gather(cursor);
+    current = next;
+    receive(next);
+}
+
+// Receive final vector and don't fetch any more.
+void Cursor::finish() {
+    receive(next);
+}
+
+// Shift the current vector left one element
+void Cursor::shiftLeft(Float& result) {
+    result = rotate(current, 15);
+    Float nextRot = rotate(next, 15);
+    Where (index() == 15)
+        result = nextRot;
+    End
+}
+
+// Shift the current vector right one element
+void Cursor::shiftRight(Float& result) {
+    result = rotate(current, 1);
+    Float prevRot = rotate(prev, 1);
+    Where (index() == 0)
+        result = prevRot;
+    End
+}
+
+#endif
---
+++ src/image_recognition/qpu_cursor.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "QPULib.h"
+
+class Cursor {
+
+public:
+
+    // Initialise to cursor to a given pointer
+    // and fetch the first vector.
+    void init(Ptr<Float> p);
+
+    // Receive the first vector and fetch the second.
+    // (prime the software pipeline)
+    void prime();
+
+    // Receive the next vector and fetch another.
+    void advance();
+
+    // Receive final vector and don't fetch any more.
+    void finish();
+
+    // Shift the current vector left one element
+    void shiftLeft(Float& result);
+
+    // Shift the current vector right one element
+    void shiftRight(Float& result);
+
+private:
+
+    Ptr<Float> cursor;
+    Float prev, current, next;
+
+};
