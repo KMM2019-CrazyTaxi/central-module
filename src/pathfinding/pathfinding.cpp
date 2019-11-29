04aae43e041a100867f2c6eed923f9f4239e81cd
Bence Nagy <bence657@gmail.com>
2019-11-29 15:26:50 +0100

    First implementation of pathfinding using Dijkstras algorithm

---
+++ src/pathfinding/pathfinding.cpp
@@ -0,0 +1,77 @@
+
+#include "pathfinding.hpp"
+
+#include <utility>
+#include <algorithm>
+
+#include "graph.hpp"
+
+
+std::vector<path_step> find_shortest_path(graph& g, int start, int end) {
+
+    // A node is a pair of ints first being cost and second is node id
+    // NOTE: Cost is first to allow default sorting 
+
+    std::unordered_map<int, int> previous;
+    std::unordered_map<int, int> dist;
+
+    // Lambda function that compares costs of nodes by binding the dist map to it
+    auto comparator = [&dist](int lhs, int rhs) { return dist[lhs] > dist[rhs]; };
+
+    // Use comparator lambda above that uses dist to compare
+    std::vector<int> queue;
+
+    // std::priority_queue<int, std::vector<int>, decltype(comparator)> queue(comparator);
+
+    dist[start] = 0;
+    queue.push_back(start);
+
+    // Initialise all other nodes to infinite cost and add them to the priority queue
+    for (int i = 0; i < g.get_nodes(); i++) {
+
+        if (i == start) continue;
+
+        dist[i] = std::numeric_limits<int>::infinity();
+        queue.push_back(i);
+    }
+
+    std::make_heap(queue.begin(), queue.end(), comparator);
+
+    while (!queue.empty()) {
+        int current = queue.front();
+        pop_heap(queue.begin(), queue.end(), comparator);
+        queue.pop_back();
+
+        for (const edge& e : g.get_edges(current)) {
+            
+            int new_dist = dist[current] + e.cost;
+
+            if (new_dist < dist[e.end]) {
+                dist[e.end] = new_dist;
+                previous[e.end] = current;
+
+                // Since costs have been updated, resort heap
+                sort_heap(queue.begin(), queue.end(), comparator);
+            }
+        }
+    }
+
+    // All shortest paths have been found, unwind path
+    int current = end;
+    std::vector<path_step> path;
+    
+    while (current != start) {
+
+        int prev = previous[current];
+
+        // Add destination node (current) and the direction for each step
+        edge e = g.get_edge(prev, current);
+        path.push_back((path_step){current, e.dir});
+
+    }
+
+    // Path is backwards so reverse it
+    std::reverse(path.begin(), path.end());
+
+    return path;
+}
---
+++ src/pathfinding/pathfinding.hpp
@@ -0,0 +1,19 @@
+#ifndef CM_PATHFINDING_H
+#define CM_PATHFINDING_H
+
+#include <vector>
+
+enum direction {
+    STRAIGHT,
+    LEFT,
+    RIGHT
+};
+
+struct path_step {
+    int node;
+    direction dir;
+};
+
+std::vector<path_step> find_shortest_path(graph& g, int start, int finish);
+
+#endif
