90d6f51b4f9e4c6bf543156d77f26a2ad6424350
Robert Sehlstedt <sehlstedt.robert@gmail.com>
2019-11-30 16:08:42 +0100

    Fixes a bug where the speed factor would become -inf or inf, making the car accelerate uncontrollably

--- src/pid_controller/systems/line.hpp
+++ src/pid_controller/systems/line.hpp
@@ -3,6 +3,9 @@
 
 #include "regulator_data.hpp"
 
+#define MAX_INPUT_ANGLE 90
+#define MAX_INPUT_SPEED 128
+
 /**
   * Line follower
   * This control systems regulates the input speed and angle to make sure
