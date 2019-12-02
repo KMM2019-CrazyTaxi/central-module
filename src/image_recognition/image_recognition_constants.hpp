af54ac451b3777a9a88fa5d72268116d03a94f9a
Robert Sehlstedt <sehlstedt.robert@gmail.com>
2019-12-02 20:20:45 +0100

    Tweaks

--- src/image_recognition/image_recognition_constants.hpp
+++ src/image_recognition/image_recognition_constants.hpp
@@ -40,7 +40,7 @@ const bool OUTPUT_MARKED_IMAGE_TO_FILE{ true };
 // Frame taken by the camera each second.
 const uint32_t CAMERA_FPS{ 60 };
 
-const uint32_t BOUND_DISTANCE_1_PIXEL{ 100 };
+const uint32_t BOUND_DISTANCE_1_PIXEL{ 75 };
 const uint32_t BOUND_DISTANCE_2_PIXEL{ 100 };
 
 const uint32_t EDGE_AVG_PIXELS{ 20 };
