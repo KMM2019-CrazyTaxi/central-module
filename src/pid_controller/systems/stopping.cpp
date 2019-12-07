15781e589999781a38ba174b669aa156a6365239
Robert Sehlstedt <sehlstedt.robert@gmail.com>
2019-12-07 17:25:56 +0100

    Improves algorithm for stopping close to a stop-line

--- src/pid_controller/systems/stopping.cpp
+++ src/pid_controller/systems/stopping.cpp
@@ -1,52 +1,56 @@
 #include "stopping.hpp"
 #include "logging.hpp"
 
-double calc_ref_speed(double, double, double, double);
+double calc_ref_speed(double, double, double, double, double);
 
 pid_system_out pid_stopping(const pid_decision_data &in) {
 
-  double kp = in.out.params.stopping.kp;
-  double ki = in.out.params.stopping.ki;
-  double kd = in.out.params.stopping.kd;
-  double alpha = in.out.params.stopping.alpha;
-  double beta = in.out.params.stopping.beta;
+    double kp = in.out.params.stopping.kp;
+    double ki = in.out.params.stopping.ki;
+    double kd = in.out.params.stopping.kd;
+    double alpha = in.out.params.stopping.alpha;
+    double beta = in.out.params.stopping.beta;
 
-  double dist = in.out.metrics.dist_stop_line;
-  double curr_speed = in.out.metrics.curr_speed;
-  double ref_speed = in.out.speed;
-  double thold = in.out.params.stopping.speed_threshold;
-  double min_dist = in.out.params.stopping.min_value;
+    double ref_speed = in.out.speed;
+    double dist = in.out.metrics.dist_stop_line;
 
-  double ref_speed_updated = calc_ref_speed(ref_speed, dist, min_dist, thold);
+    double curr_speed = in.out.metrics.curr_speed;
 
-  regulator_sample_data samples = in.out.samples;
-  double sample_d = beta * ref_speed_updated - curr_speed;
-  double dt = in.out.dt;
+    double min_dist = in.out.params.stopping.min_value;
+    double speed_cutoff = in.out.params.stopping.speed_threshold;
+    double dist_threshold = in.out.params.stopping.angle_threshold;
 
-  double calc_p = alpha * ref_speed_updated - curr_speed;
-  double calc_i = 0;
-  double calc_d = (sample_d - samples.stopping_speed_d) / dt;
+    double ref_speed_updated = calc_ref_speed(ref_speed, dist, speed_cutoff, min_dist,
+            dist_threshold);
 
-  double p = kp * calc_p;
-  double i = ki * calc_i;
-  double d = kd * calc_d;
+    regulator_sample_data samples = in.out.samples;
+    double sample_d = beta * ref_speed_updated - curr_speed;
+    double dt = in.out.dt;
 
-  samples.line_speed_d = sample_d;
+    double calc_p = alpha * ref_speed_updated - curr_speed;
+    double calc_i = 0;
+    double calc_d = (sample_d - samples.stopping_speed_d) / dt;
 
-  double res = p + i + d;
+    double p = kp * calc_p;
+    double i = ki * calc_i;
+    double d = kd * calc_d;
 
-  pid_system_out out =
-    {
-     .angle = in.out.angle,
-     .speed = curr_speed + res,
-     .samples = samples
-    };
+    samples.line_speed_d = sample_d;
 
-  return out;
+    double res = p + i + d;
+
+    pid_system_out out =
+      {
+       .angle = in.out.angle,
+       .speed = curr_speed + res,
+       .samples = samples
+      };
+
+    return out;
 }
 
-double calc_ref_speed(double ref_speed, double dist,
-                        double min_dist, double threshold) {
-    if (dist > threshold) return ref_speed;
-    return (ref_speed / (threshold - min_dist)) * (dist - min_dist);
+double calc_ref_speed(double r, double d, double v0, double d0, double dt) {
+    if (d > dt) return r;
+    if (d < d0) return 0;
+    return ( (r-v0)/(dt-d0) * d) + (r/dt) * ( (dt-d0)/(r-v0) );
 }
