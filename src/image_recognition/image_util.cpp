04d6497bcb42606f9e7c57a286aea77818ab3bff
Herman Appelgren <herap86@upp2-103.ad.liu.se>
2019-12-11 11:58:03 +0100

    Changed ir so that side edges are detected from the sides, not the middle.

--- src/image_recognition/image_util.cpp
+++ src/image_recognition/image_util.cpp
@@ -1,3 +1,5 @@
+#include <opencv2/imgcodecs.hpp>
+
 #include "image_util.hpp"
 
 void read_image(uint8_t* image, std::istream& input) {
@@ -83,7 +85,7 @@ void get_max_edge(const uint8_t* edgex_image, const uint8_t* edgey_image,
 	uint32_t strongest_edge_pixel{ row_middle };
         double min_stronger_edge_strength{ 
             strongest_strength * RELATIVE_EDGE_STRENGTH_THRESHOLD };
-	for (uint32_t column{ row_middle }; column >= 1; --column) {
+	for (uint32_t column{ 1 }; column < row_middle; ++column) {
 	    if (edgex_image[row_start + column] >= min_stronger_edge_strength) {
 		strongest_strength = edgex_image[row_start + column];
 		strongest_edge_pixel = column;
@@ -97,7 +99,7 @@ void get_max_edge(const uint8_t* edgex_image, const uint8_t* edgey_image,
 	strongest_edge_pixel = row_middle;
         min_stronger_edge_strength = 
             strongest_strength * RELATIVE_EDGE_STRENGTH_THRESHOLD;
-	for (uint32_t column{ row_middle }; column < width; ++column) {
+	for (uint32_t column{ width - 1 }; column > row_middle; --column) {
 	    if (edgex_image[row_start + column] >= min_stronger_edge_strength) {
 		strongest_strength = edgex_image[row_start + column];
 		strongest_edge_pixel = column;
