493f90978635d1c1512e9106bc9f0faabcc37a13
Herman Appelgren <herap603@student.liu.se>
2019-12-16 15:43:56 +0100

    When receiving a new route, the first location is not set as previous_pos.

--- src/network/net_message_handler.cpp
+++ src/network/net_message_handler.cpp
@@ -351,6 +351,7 @@ packet handle_send_route(const packet& p) {
     mission_data* md = (mission_data*) registry.acquire_data(MISSION_DATA_ID);
 
     md->missions = std::move(new_missions);
+    md->previous_pos = new_missions[0].first;
 
     registry.release_data(MISSION_DATA_ID);
 
