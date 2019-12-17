f3c1cd95da020d1648e1ce7b7f1fcdf1c068496a
Herman Appelgren <herap603@student.liu.se>
2019-12-17 06:46:15 +0100

    Reworked stopping system.

--- src/pid_controller/systems/stopping.cpp
+++ src/pid_controller/systems/stopping.cpp
@@ -2,22 +2,34 @@
 #include "logging.hpp"
 #include <cmath>
 
+double last_dist{ 10000 };
+const double EPS{  };
+
 pid_system_out pid_stopping(const pid_decision_data &in) {
 
     double dist = in.dist;
     double curr_speed = in.out.metrics.curr_speed;
     double speed_cutoff = in.out.params.stopping.speed_threshold;
-    double reverse_speed = in.out.params.stopping.kd;
-
     double res = 0;
 
 
     bool finished = false;
     // If we are going too fast, break
-    if (curr_speed > speed_cutoff) res = reverse_speed;
-    else if (dist > 300) res = in.out.params.stopping.kp;
+    if (last_dist < 300 && last_dist < dist - EPS)
+    {
+        finished = in.set_if_finished;
+        last_dist = 10000;
+    }
+    else if (curr_speed > speed_cutoff)
+    {
+        res = -curr_speed;
+        last_dist = dist;
     // We are setting speed to 0, should mean we might have finished
-    else if (in.set_if_finished) finished = true;
+    else if (in.set_if_finished)
+    {
+        last_dist = 10000;
+        finished = true;
+    }
 
     pid_system_out out =
       {
