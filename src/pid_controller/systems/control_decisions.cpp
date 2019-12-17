ade5af13403bc3d5f80136811029f84c222f26cf
Herman Appelgren <herap603@student.liu.se>
2019-12-17 06:42:58 +0100

    Fixed issues in control_decisions.

--- src/pid_controller/systems/control_decisions.cpp
+++ src/pid_controller/systems/control_decisions.cpp
@@ -7,7 +7,7 @@ pid_decision_return regulate(pid_decision_data &);
 
 pid_decision_return pid_decision(pid_decision_in &in) {
     pid_decision_data data = decide(in);
-
+/*
     queue_message("Path:");
     std::string path_str;
     double curr_line_height = in.metrics.dist_stop_line;
@@ -19,7 +19,7 @@ pid_decision_return pid_decision(pid_decision_in &in) {
     queue_message("CURR_LINE_HEIGHT: " + std::to_string(curr_line_height));
     queue_message("Node index: " + std::to_string(data.map.index));
     queue_message("Next node: " + std::to_string(data.map.path[data.map.index].node));
-
+*/
     pid_decision_return out = regulate(data);
     out.previous_pos = data.map.previous_pos;
     out.next_pos = data.map.next_pos;
@@ -47,14 +47,6 @@ pid_decision_data decide(pid_decision_in &in) {
         .out.speed = 10
         };
 
-    // If an obstacle is ahead, we stop
-    if (static_cast<double>(in.sensor_data.dist) < in.params.stopping.min_value) {
-        data.sys = stopping;
-        data.out.speed = 0;
-        data.dist = (double)in.sensor_data.dist;
-        return data;
-    }
-
     // If distance to stop line increased, we assume we passed one.
     // This also means that the initial sample value should be big.
     double curr_line_height = in.metrics.dist_stop_line;
@@ -67,16 +59,25 @@ pid_decision_data decide(pid_decision_in &in) {
     {
         data.map.previous_pos = in.map.path[index].node;
         index++;
+        queue_message("Detected stop line, now at index " + std::to_string(index));
     }
     data.map.next_pos = in.map.path[index].node;
     data.map.index = index;
 
     path_step next = in.map.path[index];
 
+    // If an obstacle is ahead, we stop
+    if (static_cast<double>(in.sensor_data.dist) < in.params.stopping.min_value) {
+        data.sys = stopping;
+        data.out.speed = in.metrics.curr_speed;
+        data.dist = (double)in.sensor_data.dist;
+        return data;
+    }
+
     // Approaching a stop-line, is it the end node?
     if (next.node == in.map.path.back().node && curr_line_height < in.params.stopping.min_value) {
         data.sys = stopping;
-        data.out.speed = 0;
+        data.out.speed = in.metrics.curr_speed;
         data.dist = in.metrics.dist_stop_line;
         data.set_if_finished = true;
         return data;
@@ -96,6 +97,7 @@ pid_decision_data decide(pid_decision_in &in) {
 pid_decision_return regulate(pid_decision_data &dec) {
 
   pid_system_out line_in;
+  pid_system_out stopping_out;
 
   switch(dec.sys) {
 
@@ -124,10 +126,11 @@ pid_decision_return regulate(pid_decision_data &dec) {
     {
 
       // Regulate stopping
-      pid_system_out stopping = pid_stopping(dec);
+      stopping_out = pid_stopping(dec);
 
       // set input for line system
-      line_in = stopping;
+      line_in = dec.out;
+      line_in.speed = 10;
 
     }break;
 
@@ -148,13 +151,14 @@ pid_decision_return regulate(pid_decision_data &dec) {
 
   line_in.params = dec.out.params;
   line_in.dt = dec.out.dt;
+  line_in.metrics.curr_speed = dec.out.metrics.curr_speed;
   pid_system_out line_out = pid_line(line_in);
   pid_decision_return out =
     {
      .angle = line_out.angle,
-     .speed = line_out.speed,
+     .speed = (dec.sys == stopping && std::abs(line_out.angle) < 70) ? stopping_out.speed : line_out.speed,
      .samples = line_out.samples,
-     .mission_finished = line_in.mission_finished
+     .mission_finished = (dec.sys == stopping && stopping_out.mission_finished)
     };
 
   return out;
